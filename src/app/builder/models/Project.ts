2ad777dba36f6eedeccba6b03f3c02148d150f1d
Тилек Сыдыков <tilekfromkg@gmail.com>
2021-04-09 13:38:52 +0600

    paginator fix

--- src/app/builder/models/Project.ts
+++ src/app/builder/models/Project.ts
@@ -1,9 +1,15 @@
 import {DBConnection} from './DBConnection';
+import {IHash} from '../../services/storage/storage.service';
 
 export class Project {
   ID: string = "";
   name: string = "";
 
-  endPoints: Array<string> = [];
+  endPoints: IHash<EndPointReference> = {};
   dbConnection: string = "";
+}
+
+export class EndPointReference {
+  key: string = "";
+  value: string = "";
 }
